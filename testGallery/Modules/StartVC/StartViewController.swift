ec571cec8da21eee53c185826d647f6559b0be84
Alexey <oooparkh@gmail.com>
2021-04-23 11:20:24 +0300

    Storyboard fixed

--- testGallery/Modules/StartVC/StartViewController.swift
+++ testGallery/Modules/StartVC/StartViewController.swift
@@ -7,6 +7,7 @@ class StartViewController: UIViewController {
     @IBOutlet weak private var makePhotoButton: UIButton!
     @IBOutlet weak private var galleryButton: UIButton!
     @IBOutlet weak private var saveButton: UIButton!
+    
     private let pickerController = UIImagePickerController()
     
      enum ImageSource {
